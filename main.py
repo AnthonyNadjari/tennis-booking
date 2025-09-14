88f8bf0cd594ce94101d280d2d640828831d350c
Anthony Nadjari <98424980+AnthonyNadjari@users.noreply.github.com>
2025-09-14 19:56:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ofrom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
